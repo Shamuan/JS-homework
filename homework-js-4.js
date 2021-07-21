5a27e9c01c0e65b6fb18250e991a546ad6e64d07
Hennadii Vysotskyi <86295460+Shamuan@users.noreply.github.com>
2021-07-21 19:51:42 +0300

    Update homework-js-4.js

--- homework-js-4.js
+++ homework-js-4.js
@@ -1,25 +1,28 @@
+// task 1
+
+var group = [
+    {name: "Andrii", lastName: "Rastorhuiev", age: 24, notebook: true},
+    {name: "Hennadii", lastName: "Vysotskii", age: 24, notebook: false},
+    {name: "Misha", lastName: "Solonina", age: 25, notebook: true}
+];
+
+var newGroup = []
+
+function getStudentsList (arrayOfStudents) {
+  for (var student of arrayOfStudents) {
+    console.log (`${student.name} ${student.lastName} ${student.age} ${student.notebook}`),
+    newGroup.push(`${student.name} ${student.lastName} ${student.age} ${student.notebook}`)
+  }
+  return newGroup
+}
+
 // task 2
 
 var group = [
-    {
-      name: "Vasya",
-      lastName: "Petrov",
-      age: 23,
-      notebook: false,
-  },
-  {
-      name: "Petro",
-      lastName: "vasil",
-      age: 21,
-      notebook: true,
-  },
-  {
-      name: "Mitya",
-      lastName: "Ivanov",
-      age: 24,
-      notebook: false,
-  }
-]
+    {name: "Andrii", lastName: "Rastorhuiev", age: 24, notebook: true},
+    {name: "Hennadii", lastName: "Vysotskii", age: 24, notebook: false},
+    {name: "Misha", lastName: "Solonina", age: 25, notebook: true}
+];
 
 function addNewStudent (Name, LastName, Age, Notebook) {
     var newStudent = {name: Name, lastName: LastName, age: Age, notebook: Notebook};
