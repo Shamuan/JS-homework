3f27eacb254ab9afcad0c6cce88a9bc986c0f49b
Hennadii Vysotskyi <86295460+Shamuan@users.noreply.github.com>
2021-07-24 18:28:41 +0300

    Add files via upload

---
+++ homework-js-8-recursion.js
@@ -0,0 +1,12 @@
+( function() {
+
+    for( var i = 0; i < 100; i++)
+    
+    setTimeout (
+        function () {
+            var newData = new Date()
+            console.log(newData)
+        },
+        1000 * i
+    )
+})()
