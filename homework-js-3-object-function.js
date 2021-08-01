22eaedfd58eed92c483a2642415ee80e97735df3
Hennadii Vysotskyi <vysotskiygennadiy@gmail.com>
2021-08-01 15:28:46 +0300

    upd task 2

--- homework-js-3-object-function.js
+++ homework-js-3-object-function.js
@@ -1,5 +1,34 @@
+// task 1
+
 function test () {
-  return arguments ? arguments : arguments.callee
+  console.log(arguments.callee.name, arguments)
 }
 
 test(10, false, "google")
+
+// task 2
+
+function userInfo () {
+  if(this.registered === true) {
+    console.log(`Дата регистрации: ${this.data}`)
+  }else{
+    console.log(`Незарегистрированный пользователь: ${this.name}`)
+  }
+};
+
+var Petya = {
+ name: 'Petya',
+ registered: true,
+ data: '21.11.1495'
+};
+var Manya = {
+ name: 'Manya',
+ registered: false,
+ data: '21.11.1234'
+};
+
+Petya.getInfo = userInfo
+Manya.getInfo = userInfo
+
+Petya.getInfo()
+Manya.getInfo()
